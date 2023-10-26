343471c36ce12b25e42879071159a77d2dc65cd2
Mahesace <141615910+Mahesace@users.noreply.github.com>
2023-10-26 22:00:16 +0700

    Add files via upload
    ganbate ajadah

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Halaman Login</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <h1>Halaman Login</h1>
-        <div class="form">
-        <form action="" method="post" name="form_input">
-            <div class="username">
-            username :<input type="text" name="username"placeholder="Masukan Username" required="">
-            </div>
-            <div class="password">
-            password : <input type="text" name="password" placeholder="Masukan Password"required="">
-            </div>
-            <div class="button">
-            <input type="submit" name="input" value="login">
-            <input type="reset"name="reset" value="reset">
-        </form>
-        </div>
-        </body>
-</html>    
+<?php
+$koneksi = mysqli_connect("localhost","root","","regitrasi");
+
+$nama = $_POST['nama'];
+$umur = $_POST['umur'];
+$email = $_POST['email'];
+$jeniskelamin = $_POST['jeniskelamin'];
+$agama = $_POST['agama'];
+$komentar = $_POST['komentar'];
+$submit = $_POST['submit'];
+
+
+$query = "iINSERT INTO registrasi values('$nama','$umur','$email','$jeniskelamai','$agama','$komentar','$submit')";
+mysqli_query($koneksi, $query);    ?>
